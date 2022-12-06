18e20b74f489dfee9d01e3f3d7f58436af323e2c
Benoit <benoit.zins@outlook.fr>
2022-12-06 09:29:23 +0100

    Implement odata

--- Report_App_WASM/Server/Program.cs
+++ Report_App_WASM/Server/Program.cs
@@ -19,198 +19,206 @@ using Report_App_WASM.Shared.Services;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO.Compression;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.ConfigureLogging((hostingContext, logging) =>
+internal class Program
 {
-    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-
-    logging.AddEntityFramework<ApplicationDbContext, ApplicationLogSystem>();
-
-});
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole<Guid>>()
-    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-//builder.Services.AddIdentityServer()
-//    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-builder.Services.Configure<BaseUser>(builder.Configuration.GetSection("BaseUserDefaultOptions"));
-
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddHttpContextAccessor();
-//builder.Services.AddAuthentication()
-//    .AddIdentityServerJwt();
-builder.Services.AddTransient<InitializeDatabase>();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireLowercase = false;
-
-    // Lockout settings
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
-    options.Lockout.MaxFailedAccessAttempts = 10;
-    options.Lockout.AllowedForNewUsers = true;
-
-    // User settings
-    options.User.RequireUniqueEmail = false;
-});
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.Events.OnRedirectToLogin = context =>
+    private static void Main(string[] args)
     {
-        context.Response.StatusCode = 401;
-        return Task.CompletedTask;
-    };
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddLocalization(options => options.ResourcesPath = "Utils/LanguageRessources");
-builder.Services.AddSingleton<CommonLocalizationService>();
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = new[]
-     {
+        builder.Host.ConfigureLogging((hostingContext, logging) =>
+        {
+            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+
+            logging.AddEntityFramework<ApplicationDbContext, ApplicationLogSystem>();
+
+        });
+
+        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole<Guid>>()
+            .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+        //builder.Services.AddIdentityServer()
+        //    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        builder.Services.Configure<BaseUser>(builder.Configuration.GetSection("BaseUserDefaultOptions"));
+
+
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        builder.Services.AddHttpContextAccessor();
+        //builder.Services.AddAuthentication()
+        //    .AddIdentityServerJwt();
+        builder.Services.AddTransient<InitializeDatabase>();
+
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+            // Password settings
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = false;
+
+            // Lockout settings
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            options.Lockout.MaxFailedAccessAttempts = 10;
+            options.Lockout.AllowedForNewUsers = true;
+
+            // User settings
+            options.User.RequireUniqueEmail = false;
+        });
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.Events.OnRedirectToLogin = context =>
+            {
+                context.Response.StatusCode = 401;
+                return Task.CompletedTask;
+            };
+        });
+
+        builder.Services.AddLocalization(options => options.ResourcesPath = "Utils/LanguageRessources");
+        builder.Services.AddSingleton<CommonLocalizationService>();
+        builder.Services.Configure<RequestLocalizationOptions>(options =>
+        {
+            var supportedCultures = new[]
+             {
                     new CultureInfo("fr"),
                     new CultureInfo("en"),
                     new CultureInfo("de"),
                     new CultureInfo("nl"),
-                 };
-    options.DefaultRequestCulture = new RequestCulture("en");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
+                         };
+            options.DefaultRequestCulture = new RequestCulture("en");
+            options.SupportedCultures = supportedCultures;
+            options.SupportedUICultures = supportedCultures;
+        });
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+        builder.Services.AddControllersWithViews().AddOData(opt => opt.Count().Filter().Expand().Select().OrderBy().SetMaxTop(100)/* other config */);
+        builder.Services.AddRazorPages();
 
-// Add Hangfire services.
-builder.Services.AddHangfire(configuration => configuration
-    .UseDarkModeSupportForDashboard()
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection"), new SqlServerStorageOptions
-    {
-        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-        QueuePollInterval = TimeSpan.Zero,
-        UseRecommendedIsolationLevel = true,
-        DisableGlobalLocks = true
-    }));
+        // Add Hangfire services.
+        builder.Services.AddHangfire(configuration => configuration
+            .UseDarkModeSupportForDashboard()
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection"), new SqlServerStorageOptions
+            {
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.Zero,
+                UseRecommendedIsolationLevel = true,
+                DisableGlobalLocks = true
+            }));
 
-// Add the processing server as IHostedService
-builder.Services.AddHangfireServer(options =>
-{
-    options.Queues = new[] { "default", "report", "cleaner", "alert", "datatransfer" };
-});
-builder.Services.AddDirectoryBrowser();
+        // Add the processing server as IHostedService
+        builder.Services.AddHangfireServer(options =>
+        {
+            options.Queues = new[] { "default", "report", "cleaner", "alert", "datatransfer" };
+        });
+        builder.Services.AddDirectoryBrowser();
 
 
-//enable response compresssion 
-builder.Services.AddResponseCompression(options =>
-{
-    options.Providers.Add<BrotliCompressionProvider>();
-    options.Providers.Add<GzipCompressionProvider>();
-    options.MimeTypes =
-        ResponseCompressionDefaults.MimeTypes.Concat(
-            new[] { "image/svg+xml" });
-});
+        //enable response compresssion 
+        builder.Services.AddResponseCompression(options =>
+        {
+            options.Providers.Add<BrotliCompressionProvider>();
+            options.Providers.Add<GzipCompressionProvider>();
+            options.MimeTypes =
+                ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "image/svg+xml" });
+        });
 
-builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
-{
-    options.Level = CompressionLevel.Fastest;
-});
+        builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+        {
+            options.Level = CompressionLevel.Fastest;
+        });
 
-builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-{
-    options.Level = CompressionLevel.Fastest;
-});
+        builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+        {
+            options.Level = CompressionLevel.Fastest;
+        });
 
-var mapperConfig = new MapperConfiguration(mc =>
-{
-    mc.AddProfile(new MappingProfile());
-});
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new MappingProfile());
+        });
 
-IMapper mapper = mapperConfig.CreateMapper();
-builder.Services.AddSingleton(mapper);
+        IMapper mapper = mapperConfig.CreateMapper();
+        builder.Services.AddSingleton(mapper);
 
-var app = builder.Build();
+        var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
+        using (var scope = app.Services.CreateScope())
+        {
 
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
-        var dbInit = services.GetRequiredService<InitializeDatabase>();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+                var dbInit = services.GetRequiredService<InitializeDatabase>();
 
-        dbInit.InitializeAsync().Wait();
-        HashKey.Key = context.ApplicationUniqueKey.OrderBy(a => a.Id).Select(a => a.Id.ToString().Replace("-", "")).FirstOrDefault();
-        var parameters = context.ApplicationParameters.FirstOrDefault();
-        ApplicationConstants.ApplicationName = parameters.ApplicationName;
-        ApplicationConstants.ApplicationLogo = parameters.ApplicationLogo;
+                dbInit.InitializeAsync().Wait();
+                HashKey.Key = context.ApplicationUniqueKey.OrderBy(a => a.Id).Select(a => a.Id.ToString().Replace("-", "")).FirstOrDefault();
+                var parameters = context.ApplicationParameters.FirstOrDefault();
+                ApplicationConstants.ApplicationName = parameters.ApplicationName!;
+                ApplicationConstants.ApplicationLogo = parameters.ApplicationLogo!;
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+        }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseResponseCompression();
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseHangfireDashboard("/Hangfire", new DashboardOptions
+        {
+            Authorization = new[] { new HangfireAuthorizationFilter() }
+        });
+
+        app.UseHangfireDashboard("/HangfireRead", new DashboardOptions
+        {
+            IsReadOnlyFunc = (context) => true,
+            Authorization = new[] { new HangfireAuthorizationFilterRead() }
+        });
+
+        app.MapRazorPages();
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
     }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseResponseCompression();
-app.UseHttpsRedirection();
-app.UseRequestLocalization();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseHangfireDashboard("/Hangfire", new DashboardOptions
-{
-    Authorization = new[] { new HangfireAuthorizationFilter() }
-});
-
-app.UseHangfireDashboard("/HangfireRead", new DashboardOptions
-{
-    IsReadOnlyFunc = (DashboardContext context) => true,
-    Authorization = new[] { new HangfireAuthorizationFilterRead() }
-});
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
