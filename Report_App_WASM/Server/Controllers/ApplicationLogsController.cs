0ba62b850c8d736f0f02cd9e6426a5a70727433b
Benoit <benoit.zins@outlook.fr>
2022-12-09 20:42:56 +0100

    proof of concept 2

--- Report_App_WASM/Server/Controllers/ApplicationLogsController.cs
+++ Report_App_WASM/Server/Controllers/ApplicationLogsController.cs
@@ -2,17 +2,20 @@
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
 using Microsoft.EntityFrameworkCore;
 using Report_App_WASM.Server.Data;
+using Report_App_WASM.Server.Models;
 using Report_App_WASM.Shared;
 using Report_App_WASM.Shared.DTO;
 
 namespace Report_App_WASM.Server.Controllers
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class ApplicationLogsController : Controller
+
+    public class ApplicationLogsController : ODataController
     {
         private readonly ILogger<ApplicationLogsController> _logger;
         private readonly ApplicationDbContext _context;
@@ -26,11 +29,26 @@ namespace Report_App_WASM.Server.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet("SystemLogs")]
-        public async Task<IEnumerable<ApplicationLogSystemDTO>> GetSystemLogs()
+        [EnableQuery()]
+        [Route("odata/SystemLogs")]
+        public IQueryable<ApplicationLogSystem> GetSystemLogs()
         {
-            _logger.LogWarning("GridLoaded");
-            return await _context.ApplicationLogSystem.AsNoTracking().ProjectTo<ApplicationLogSystemDTO>(_mapper.ConfigurationProvider).ToArrayAsync();
+            return  _context.ApplicationLogSystem.AsNoTracking();
         }
+
+        [EnableQuery()]
+        [Route("odata/SystemLogs{id}")]
+        public IQueryable<ApplicationLogSystem> GetSystemLogs(ODataQueryOptions odataQueryOptions, [FromODataUri] int key)
+        {
+            Console.WriteLine("start");
+            var q = _context.ApplicationLogSystem.AsNoTracking();
+            q = (IQueryable<ApplicationLogSystem>)odataQueryOptions.ApplyTo(q,AllowedQueryOptions.Top|AllowedQueryOptions.Skip);
+
+            var j=q.Take(10000).ToList();
+            Console.WriteLine("result nbr of rows "+j.Count);
+            Console.WriteLine("end");
+            return q;
+        }
+
     }
 }
---
+++ Report_App_WASM/Shared/ApiResponse/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using Report_App_WASM.Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Report_App_WASM.Shared
+{
+    public class ApiResponse<T> where T:class
+    {
+        [JsonPropertyName("@odata.count")]
+        public int Count { get; set; }
+        [JsonPropertyName("value")]
+        public List<T> Value { get; set; }
+    }
+}
