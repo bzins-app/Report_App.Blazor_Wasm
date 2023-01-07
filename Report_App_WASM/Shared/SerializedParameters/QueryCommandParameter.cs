b90ebfe4013de515ebf38d405b108e3e3a3542ea
Benoit <benoit.zins@outlook.fr>
2023-01-07 08:44:29 +0100

    Improve query parameters

--- Report_App_WASM/Shared/SerializedParameters/QueryCommandParameter.cs
+++ Report_App_WASM/Shared/SerializedParameters/QueryCommandParameter.cs
@@ -2,10 +2,13 @@
 {
     public class QueryCommandParameter
     {
-        public string? ParameterIdentifier { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
+        public int DisplaySequence{ get; set; } = 99;
+        public string ParameterIdentifier { get; set; } = string.Empty;
         public QueryCommandParameterValueType ValueType { get; set; }
         public CalulatedDateOption DateOption { get; set; }
         public bool Required { get; set; } = true;
+        public bool HideParameter { get; set; } = false;
         public string Value { get; set; } = string.Empty;
     }
 
