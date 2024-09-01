14de152175fac95cc5338d2cdc4bf046995c606d
Benoit <benoit.zins@outlook.fr>
2024-09-01 07:38:08 +0200

    #server:ConnectionString add "SslMode=Preferred;" for MySQL and MariaDb (hardcoded)

--- Report_App_WASM/Server/Utils/RemoteDb/MariaDbRemoteDb.cs
+++ Report_App_WASM/Server/Utils/RemoteDb/MariaDbRemoteDb.cs
@@ -168,7 +168,7 @@ public class MariaDbRemoteDb : IRemoteDb
         var databaseInfo = "";
         if (dbInfo.UseDbSchema) databaseInfo = $";database={dbInfo.DbSchema}";
         value.ConnnectionString =
-            $"server={dbInfo.ConnectionPath};port={dbInfo.Port}{databaseInfo};uid={dbInfo.ConnectionLogin};password={EncryptDecrypt.EncryptDecrypt.DecryptString(dbInfo.Password)};";
+            $"server={dbInfo.ConnectionPath};port={dbInfo.Port}{databaseInfo};uid={dbInfo.ConnectionLogin};password={EncryptDecrypt.EncryptDecrypt.DecryptString(dbInfo.Password)};SslMode=Preferred;";
 
         return value;
     }
