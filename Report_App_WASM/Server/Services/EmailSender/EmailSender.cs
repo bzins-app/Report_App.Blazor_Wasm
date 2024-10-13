6c7803c90fb6508a820ba409d492014dc4eabf0d
Benoit <benoit.zins@outlook.fr>
2024-10-13 02:00:43 +0200

    #code suppress some warnings

--- Report_App_WASM/Server/Services/EmailSender/EmailSender.cs
+++ Report_App_WASM/Server/Services/EmailSender/EmailSender.cs
@@ -115,11 +115,11 @@ public class EmailSender : IEmailSender
         {
             if (recipient.Bcc)
             {
-                message.Bcc.Add(new MailAddress(recipient.Email!, recipient.Email));
+                message.Bcc.Add(new MailAddress(recipient.Email, recipient.Email));
             }
             else
             {
-                message.To.Add(new MailAddress(recipient.Email!, recipient.Email));
+                message.To.Add(new MailAddress(recipient.Email, recipient.Email));
             }
         }
 
