d2114d80648312b1ed0882766e69c8bedbd792de
Benoit <benoit.zins@outlook.fr>
2025-01-03 12:27:41 +0100

    #server change app version

--- Report_App_WASM/Client/Utils/ApplicationInfo.cs
+++ Report_App_WASM/Client/Utils/ApplicationInfo.cs
@@ -2,6 +2,6 @@
 
 public static class ApplicationInfo
 {
-    public const string ApplicationVersion = "202411";
-    public const int VersionYear = 2024;
+    public const string ApplicationVersion = "202501Beta";
+    public const int VersionYear = 2025;
 }
