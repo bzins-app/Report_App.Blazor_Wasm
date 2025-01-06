28227a030c0b0e6ecf9e5bcf2260a22f97bf97fc
Benoit <benoit.zins@outlook.fr>
2025-01-06 11:30:28 +0100

    Initial test with migration

--- Report_App_WASM/Server/Data/Migrations/20250105171452_testNewModel.Designer.cs
+++ Report_App_WASM/Server/Data/Migrations/20250105171452_testNewModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Report_App_WASM.Server.Data;
 
@@ -11,9 +12,11 @@ using Report_App_WASM.Server.Data;
 namespace ReportAppWASM.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250105171452_testNewModel")]
+    partial class testNewModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
